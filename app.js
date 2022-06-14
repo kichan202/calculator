36f8a8dc326af8fe395664e90598485eb920e026
francisco <franciscodorset@hotmail.com>
2022-06-14 14:29:52 -0400

    completed the calculation with eval function, can make improvements but project is just for getting familiar

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 //selectors
+const result = document.getElementById("result");
 
 //events listner
 
 //functions
+function liveScreen(enteredValue){
+    result.value += enteredValue;
+}
+
+function calculate(value){
+    try {
+        const calculatedValue = eval(value || null );
+        result.value= calculatedValue;
+        //
+        
+    } catch (error) {
+        result.value = "Bad operation";
+        setTimeout(()=>{
+            result.value = "";
+        }, 1300);
+    }
+   
+
+}
